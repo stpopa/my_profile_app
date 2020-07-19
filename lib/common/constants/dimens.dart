432b303bf9d9a0534c0eeab8ba70da3daf7eab6e
Cristian Cosneanu <cosneanuc@gmail.com>
2020-07-19 23:33:33 +0300

    Revert "Core skills screen - Implement Slider widget"
    This reverts commit dc825e1410d616f2ac78ffb216707c990975f8b6.

--- lib/common/constants/dimens.dart
+++ lib/common/constants/dimens.dart
@@ -10,15 +10,8 @@ class Dimens {
   static const double circularButtonIconSize = 30.0;
   static const double progressBarRadius = 104.0;
   static const double progressBarLineWidth = 7.0;
-  static const double skillIconSize = 50.0;
-  static const double sliderTrackHeight = 8.0;
-  static const double appBarSize = 120.0;
-  static const double addIconSize = 24.0;
-  static const double thumbWidth = 70.0;
-  static const double thumbHeight = 20.0;
 
   /// Spacing
-  static const double spacingTiny = 5.0;
   static const double spacingXSmall = 8.0;
   static const double spacingSmall = 12.0;
   static const double spacingMedium = 16.0;
@@ -26,11 +19,8 @@ class Dimens {
   static const double spacingHuge = 40.0;
 
   /// Elevation
-  static const double cardBorderRadius = 8.0;
   static const double cardElevation = 2.0;
 
   /// Font size
   static const double fontSizeMedium = 16.0;
-  static const double fontSizeNormal = 18.0;
-  static const double fontSizeLarge = 20.0;
 }
