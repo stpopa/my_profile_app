dd99e3ef4a9a70548f2132b48d1ad1aae26dd208
Cristian Cosneanu <cosneanuc@gmail.com>
2020-07-19 23:33:33 +0300

    Revert "Core skills screen - Implement screen UI"
    This reverts commit 7e4683e461b25916e6d5063ccf73d26fc7d906cf.

--- lib/modules/login/components/header_shape.dart
+++ lib/modules/login/components/header_shape.dart
@@ -1,4 +1,3 @@
-import 'package:endava_profile_app/modules/common/constants/palette.dart';
 import 'package:flutter/material.dart';
 
 class HeaderShape extends StatelessWidget {
@@ -8,7 +7,7 @@ class HeaderShape extends StatelessWidget {
       width: MediaQuery.of(context).size.width - 50,
       height: MediaQuery.of(context).size.height / 1.8,
       decoration: BoxDecoration(
-        color: Palette.wildSand,
+        color: Color.fromARGB(255, 246, 246, 246),
         borderRadius: BorderRadius.only(
           topRight: Radius.circular(350),
           bottomRight: Radius.circular(350),
