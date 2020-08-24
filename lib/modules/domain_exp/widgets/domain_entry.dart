cfd7180419f936479d45b0ef408a432ae37456be
Mihai Petrenco <mihai.petrenco@endava.com>
2020-08-25 02:38:10 +0300

    Added confirmation dialogs when deleting entries or returning without saving

--- lib/modules/domain_exp/widgets/domain_entry.dart
+++ lib/modules/domain_exp/widgets/domain_entry.dart
@@ -1,4 +1,5 @@
 import 'package:endava_profile_app/common/constants/dimens.dart';
+import 'package:endava_profile_app/common/constants/palette.dart';
 import 'package:endava_profile_app/modules/domain_exp/bloc/domain_exp_events.dart';
 import 'package:endava_profile_app/modules/domain_exp/bloc/domain_exp_provider.dart';
 import 'package:flutter/material.dart';
@@ -19,6 +20,44 @@ class DomainEntry extends StatelessWidget {
   Widget build(BuildContext context) {
     final bloc = DomainExpProvider.of(context);
 
+    void _deleteEntry(String id) {
+      showDialog(
+          context: context,
+          builder: (bctx) {
+            return AlertDialog(
+              title: Text('Delete Experience?'),
+              content: Text(
+                  'Once you delete this, you won\'t be able to get it back.'),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text(
+                    'Cancel',
+                    style: TextStyle(
+                      color: Palette.gray,
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                FlatButton(
+                  child: Text(
+                    'Confirm',
+                    style: TextStyle(
+                      color: Palette.cinnabar,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  onPressed: () {
+                    bloc.addEvent(DeleteEntry(id: id));
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          });
+    }
+
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: Dimens.spacingSmall),
       child: Column(
@@ -38,7 +77,7 @@ class DomainEntry extends StatelessWidget {
               ),
               SizedBox(width: Dimens.spacingMedium),
               GestureDetector(
-                onTap: () => bloc.addEvent(DeleteEntry(id: this.id)),
+                onTap: () => _deleteEntry(this.id),
                 child: SizedIcon.asset('assets/images/delete.png'),
               ),
             ],
